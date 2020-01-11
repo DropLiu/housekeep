838ad525ab29ae1f84012ae712226089bc69b28f
dropliu <571439942@qq.com>
2020-01-11 18:43:12 +0800

    Initial Commit

---
+++ pages/index/index.js
@@ -0,0 +1,51 @@
+Page({
+  data: {
+    swiper:{
+      autoplay: true,
+      interval: 2000,
+      duration: 500,
+      circular: true,
+      list:[
+        {
+          img:"http://515046.s81i.faiusr.com/2/101/AFEI5rcfEAIYACDyk83kBSi44r_mBTDuBTjoAkBl.jpg"
+        },
+        {
+          img:"http://515046.s81i.faiusr.com/2/101/AFEI5rcfEAIYACD0k83kBSiwhLScBjDuBTjoAkBl.jpg"
+        }
+      ]
+    },
+    hotService:{
+      list:[
+        {
+          title:"房屋维修",
+          desc:"房屋管道开换锁 大平台全保障",
+          promotePrice: 180.00,
+          originalPrice: 200,
+          book: 3,
+          remain: 97,
+          img:"http://515046.s81i.faiusr.com/4/101/AFEI5rcfEAQYACDlkonhBSjQuM_DATCMBjj8AkBl.png"
+        },
+        {
+          title:"家电维修",
+          desc:"家用电器电脑 大平台全保障",
+          promotePrice: 100.00,
+          originalPrice: 110,
+          book: 2,
+          remain: 98,
+          img:"http://515046.s81i.faiusr.com/4/101/AFEI5rcfEAQYACDikonhBSi7mpjIBDCMBjj8AkBl.png"
+        },
+        {
+          title:"家庭保洁",
+          desc:"专业培训上岗 100%健康审核",
+          promotePrice: 70.00,
+          originalPrice: 90,
+          book: 1,
+          remain: 99,
+          img:"http://515046.s81i.faiusr.com/4/101/AFEI5rcfEAQYACDokonhBSiYg5v5AjCMBjj8AkBl.png"
+        }
+      ]
+    }
+    
+  },
+
+})
